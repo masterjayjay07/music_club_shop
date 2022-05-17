0dad042bad3fe7e59a28e92bce0a6a07603783e0
Johão Pedro <johaopedro99@gmail.com>
2022-05-17 18:05:34 -0300

    feat: interface address was created

--- src/controllers/address/addressCreate.controller.ts
+++ src/controllers/address/addressCreate.controller.ts
@@ -1,3 +1,13 @@
-const addressCreateController = async () => {};
+import { Request, Response } from "express";
+import AppError from "../../errors/AppError";
+
+const addressCreateController = async (req: Request, res: Response) => {
+  try {
+  } catch (err) {
+    if (err instanceof AppError) {
+      throw new AppError(err.message, err.statusCode);
+    }
+  }
+};
 
 export default addressCreateController;
