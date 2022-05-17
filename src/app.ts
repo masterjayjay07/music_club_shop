db382a58ea55e10d1c88edc55607bb3ce29b941a
Nicollas Anderson <nicollas_sdias@hotmail.com>
2022-05-17 11:50:16 -0300

    feat: added errors middleware and added some dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { errorMiddleware } from "../src/middlewares/error.middleware";
 import router from "./routes";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use('',router)
+app.use("/", router);
+app.use(errorMiddleware);
 
-export default app
+export default app;
