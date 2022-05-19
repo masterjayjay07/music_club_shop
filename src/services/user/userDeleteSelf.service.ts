f390aaa6b04488035defdf34a4095f21ba3c8c4d
Gabriel Rocha <gabrielrs7@outlook.com>
2022-05-19 19:26:43 -0300

    fix: adjustment in middlewares authtoken and verification if its admin or owner

--- src/services/user/userDeleteSelf.service.ts
+++ src/services/user/userDeleteSelf.service.ts
@@ -1,15 +1,21 @@
 import AppDataSource from "../../data-source";
 import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/AppError";
 import { UserDataParams } from "../../interfaces";
 
 const userDeleteService = async ({ id }: UserDataParams) => {
+
   const userRepository = AppDataSource.getRepository(User);
 
   const users = await userRepository.find();
 
   const account = users.find((user) => user.id === id);
 
-  await userRepository.delete(account!.id);
+  if(!account){
+    throw new AppError(404,"User doesnt exists")
+  }
+
+  await userRepository.delete(account.id);
 
   return true;
 };
