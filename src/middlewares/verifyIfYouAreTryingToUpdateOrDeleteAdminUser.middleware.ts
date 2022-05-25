354d727ac2e4692b7c193d1a87c7a4146be47a08
Gabriel Rocha <gabrielrs7@outlook.com>
2022-05-25 11:11:19 -0300

    feat: creating new middleware to verify if you are trying to update or delete another admin user

---
+++ src/middlewares/verifyIfYouAreTryingToUpdateOrDeleteAdminUser.middleware.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError, handleError } from "../errors/AppError";
+import jwt from "jsonwebtoken";
+import AppDataSource from "../data-source";
+import Order from "../entities/order.entity";
+import { User } from "../entities/user.entity";
+
+interface IToken {
+  email: string;
+  is_adm: boolean;
+  sub: string;
+}
+
+const verifyIfYouAreTryingToUpdateOrDeleteAdminUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const userRepository = AppDataSource.getRepository(User)
+  const user = await userRepository.findOne({where:{id}})
+
+  let token: string = req.headers.authorization || "";
+  token = token.replace("Bearer ", "");
+
+  const secretKey = process.env.POSTGRES_SECRET_KEY || "secret";
+  const decoded = jwt.verify(token, secretKey) as IToken;
+
+  const { sub } = decoded;
+  const userId = sub;
+
+  if (id !== userId && user?.is_adm === true) {
+    const errorCatched = new AppError(401, `You can't update or delete another admin user`);
+    handleError(errorCatched, res);
+  }
+
+  next();
+};
+export default verifyIfYouAreTryingToUpdateOrDeleteAdminUser;
